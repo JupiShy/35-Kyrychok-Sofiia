aaf8f0d5edc65fe6f0ec02a4c7ebff02636a22d9
megas <megas@DESKTOP-819HFTM>
2024-03-01 19:49:19 +0200

    test commit

---
+++ src/practicapplication/PracticApplication.java
@@ -0,0 +1,18 @@
+package practicapplication;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Киричок Софія
+ */
+public class PracticApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
