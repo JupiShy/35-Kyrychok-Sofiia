8ba07ec4308b1923e113afcaade088554cc0d877
megas <megas@JupiShy>
2024-03-04 15:40:39 +0200

    Revert "Task 2.2"
    This reverts commit 040b3485b7ca1b51d3eed00834ac29c83073e745.

--- src/practicapplication/PracticApplication.java
+++ src/practicapplication/PracticApplication.java
@@ -1,10 +1,7 @@
 package practicapplication;
 
 import Task2_1.*;
-import Task2_2.*;
-import java.io.*;
 import java.util.Scanner;
-
 /**
  *
  * @author Киричок Софія
@@ -15,28 +12,20 @@ public class PracticApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Task 1 Test
-
+//      Task 1 Test
+        
         System.out.println("Task 1\nEnter something in console: ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println("Your input: " + input);
-
-        // Task 2 #1 Test
+        
+//      Task 2 #1 Test
+        
         System.out.println("Task 2.1");
         Parameters params = new Parameters(15, 2);
         Calculating calc = new Calculating(params);
         calc.calculate();
         String str = params.toString();
         System.out.println(str);
-
-        // Task 2 #2 Test
-        System.out.println("Task 2.2");
-        
-        SavingAndRestoring object = new SavingAndRestoring(14, "Sofiia");
-        System.out.println("Before serialization:\n" + object.toString());
-        
-        object.SerAndDeser(); 
-        
     }
 }
